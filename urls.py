faf1982a004587af2977ed60a82ce237f6790fbf
dewrekiran <166015115+dewrekiran@users.noreply.github.com>
2024-08-21 12:53:02 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,26 @@
-from django.urls import path
-from .views import ClientListCreateView, ClientDetailView, ProjectCreateView, UserProjectsView
+"""
+URL configuration for machine_test project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# machine_test/urls.py
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('clients/', ClientListCreateView.as_view(), name='client-list-create'),
-    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client-detail'),
-    path('clients/<int:pk>/projects/', ProjectCreateView.as_view(), name='project-create'),
-    path('projects/', UserProjectsView.as_view(), name='user-projects'),
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),  # This includes the URLs from the core app
 ]
+
